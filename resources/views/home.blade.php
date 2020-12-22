4cf0059459536c7d30b7a254b9df4709797301ce
Marco Guidi <mark.88g@gmail.com>
2020-12-22 15:29:28 +0100

    correzione sintassi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset("css/app.css") }}">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
         <div class="flex-center position-ref full-height">
